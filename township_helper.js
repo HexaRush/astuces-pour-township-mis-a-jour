ea7b94def2ddcc519a7e8dfb4c3ab481836a647f
HexaRush <becciarelli24@virgilio.it>
2025-06-20 14:33:51 +0200

    Create township_helper.js

---
+++ township_helper.js
@@ -0,0 +1,14 @@
+// township_helper.js
+// Script technique simulé pour Township
+
+console.log("Initialisation de l'assistant Township...");
+
+const ressources = ["blé", "lait", "bois", "pièces"];
+
+function analyserRessources() {
+    ressources.forEach(r => {
+        console.log(`Ressource détectée : ${r}`);
+    });
+}
+
+analyserRessources();
